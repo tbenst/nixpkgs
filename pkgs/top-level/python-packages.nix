34eec632293e0166be9410044851a6ea819a35b7
tbenst <lensman@tylerbenster.com>
2019-10-31 13:51:06 -0700

    python3packages.colorspacious: init at 1.1.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1651,6 +1651,8 @@ in {
 
   colorlog = callPackage ../development/python-modules/colorlog { };
 
+  colorspacious = callPackage ../development/python-modules/colorspacious { };
+
   colour = callPackage ../development/python-modules/colour {};
 
   configshell = callPackage ../development/python-modules/configshell { };
