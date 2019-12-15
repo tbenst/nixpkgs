8b491651c1afb06bddfe894e0613e0160061c2c0
tbenst <lensman@tylerbenster.com>
2019-12-15 14:27:10 -0800

    pythonPackages.pytorch-lightning: init at 0.5.3.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1226,6 +1226,8 @@ in {
 
   pytimeparse =  callPackage ../development/python-modules/pytimeparse { };
 
+  pytorch-lightning = callPackage ../development/python-modules/pytorch-lightning { };
+
   pytricia =  callPackage ../development/python-modules/pytricia { };
 
   pytrends = callPackage ../development/python-modules/pytrends { };
