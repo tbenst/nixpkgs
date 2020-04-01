535506a9ec831c8127d1019a7440e2b83762e958
tbenst <lensman@tylerbenster.com>
2020-03-31 18:29:24 -0700

    deeplabcut: init at 2.1.6.4

---
+++ pkgs/applications/science/machine-learning/deeplabcut/default.nix
@@ -0,0 +1,51 @@
+{ lib, writeText
+, wrapGAppsHook
+, opencv3
+, python3
+, mkl
+ }:
+
+let
+  pythonOverrides = python-self: python-super: {
+    # openblas does not work https://github.com/AlexEMG/DeepLabCut/issues/637#issuecomment-606893058
+    numpy = python-super.numpy.override { blas = mkl; };
+
+    # imgaug test stalls...
+    imgaug = python-super.imgaug.overrideAttrs (old:{
+      doInstallCheck = false;
+    });
+
+    opencv3 = python-super.toPythonModule (opencv3.override {
+      enablePython = true;
+      enableFfmpeg = true;
+      pythonPackages = python-self;
+    });
+  };
+
+  python = python3.override {
+    packageOverrides = pythonOverrides;
+  };
+  py = python.pkgs;
+in
+py.toPythonApplication
+  (py.deeplabcut.overridePythonAttrs(old: rec {
+  pname = "deeplabcut-gui";
+
+  script = writeText "deeplabcut"
+    ''
+      #!/usr/bin/env python
+      import deeplabcut
+      deeplabcut.launch_dlc()
+    '';
+
+  postInstall = ''
+    ${old.postInstall}
+    mkdir -p $out/bin
+    cp ${script} $out/bin/deeplabcut
+    chmod +x $out/bin/deeplabcut
+  '';
+
+  nativeBuildInputs = old.nativeBuildInputs ++ [
+    wrapGAppsHook
+  ];
+}))
